f7348654e85ee8129a6b219ebf0ecd9b9b00007c
MIEGAS!!!! <miegas4@users.noreply.github.com>
2016-05-05 11:41:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,3 @@
 /*  Criar variavel global para fazer a contagem do nr de hashtags diferentes e a hashtag com o maior numero de ocurrencias
+    Meter o que nas ADT's??? Perguntar a alguem que saiba (prof se puderes :p)
 */
