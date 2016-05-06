b34830b3bc8e76f1ab8ab3a762796f82772c3921
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2016-05-06 16:46:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include "ArvoreBinaria.h"
+#include "funcoes_aux.h"
+#include "Item.h"
 
 #define NUMSEP 11
 #define MAX 140
+
 
 int Total_Hashtags;
 int Total_Ocorrencias; // Counting Sort na Arvore    ->    M = Total_Ocorrencias
@@ -12,15 +16,16 @@ link global_h = NULL;
 static const char separators[] = {' ', '\t',',',';','.','?','!','"','\n',':','\0'};
 
  
+void split(char *line);
 
- int main()
- {
+int main()
+{
   char input = 'U'; /* Inicialiazada a 'U' para entrar no ciclo
              apenas 1 vez. Não corresponde a nenhuma
              funcao. */
-
-   Total_Hashtags = 0;
-   Total_Ocorrencias = 0;
+  int i; 
+  Total_Hashtags = 0;
+  Total_Ocorrencias = 0;
              
   char mensagem[MAX + 1];
       
@@ -40,24 +45,21 @@ static const char separators[] = {' ', '\t',',',';','.','?','!','"','\n',':','\0
           printf("%d %d\n", Total_Hashtags, Total_Ocorrencias);        
           break;
         
-        case 'm':   
+        case 'm':  
           
           printf("%s %d\n",  Maior.tag, Maior.count);
           break;
         
         case 'l':   
           //Imprime todas as Hashtags e respetivos n de ocorrencias por ordem decrescente (em caso de empate, por ordem alfabetica)
-          int i;	
           for (i = 0; i < Total_Hashtags; ++i)
           {
-          	printf("%s %d\n", nome_hash_ord(i), numero_ocor_ord(i));
+            //printf("%s %d\n", nome_hash_ord(i), numero_ocor_ord(i));
           }
           break;
                     
           
-        default:
-        
-          printf("Comando nao reconhecido, por favor reintroduza a operacao.\n");
+      
         
       }
     
@@ -65,4 +67,26 @@ static const char separators[] = {' ', '\t',',',';','.','?','!','"','\n',':','\0
  
     return 0;
     
+ }
+
+
+void split(char *line)
+{
+  
+  char *token = strtok(line, separators);
+   while(token != NULL) 
+  {
+    if(token[0] == '#')
+    {
+      Total_Ocorrencias++;
+      avalia_hash(token); // Confirmar se a hashtag foi inserido ou se ja exestia, no caso de ser inserido incrementa o Total_Hashtags
+      int i;
+
+      for(i = 1; token[i] != '\0'; i++)
+      {
+        token[i] = tolower(token[i]);
+      }      
+    }
+    token = strtok(NULL, separators);
   }
+}
