72e4c90b210151e81ae3e75976a404d28795d9ce
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2016-05-06 14:01:50 +0100

    Update funcoes_aux.h

--- funcoes_aux.h
+++ funcoes_aux.h
@@ -1,1 +1,12 @@
+#ifndef _FUNCOES_AUX_
+#define _FUNCOES_AUX_
+#ifndef _STRING_
+#define _STRING_
 
+#include "funcoes_aux.c"
+
+void avalia_hash(char *token);
+void split(char *line);
+
+#endif
+#endif
