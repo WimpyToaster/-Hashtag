76a7eee6b0808549c86f010a2aa75fcfcf767a8f
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2016-05-06 16:49:12 +0100

    Update funcoes_aux.h

--- funcoes_aux.h
+++ funcoes_aux.h
@@ -2,8 +2,13 @@
 #define _FUNCOES_AUX_
 
 #include <string.h>
+#include "ArvoreBinaria.h"
+#include "Item.h"
+
+extern int Total_Hashtags;
+extern link global_h;
+extern Item Maior;
 
 void avalia_hash(char *token);
-void split(char *line);
 
 #endif
