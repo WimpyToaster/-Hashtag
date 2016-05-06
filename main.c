e90a58956f88d063018c24b36d3de6d06412260b
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2016-05-06 11:39:28 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,96 @@
-/*  Criar variavel global para fazer a contagem do nr de hashtags diferentes e a hashtag com o maior numero de ocurrencias
-    Meter o que nas ADT's??? Perguntar a alguem que saiba (prof se puderes :p)
-*/
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define NUMSEP 11
+#define MAX 140
+
+int Total_Hashtags;
+int Total_Ocorrencias; // Counting Sort na Arvore    ->    M = Total_Ocorrencias
+Item Maior;
+
+
+
+
+static const char separators[] = {' ', '\t',',',';','.','?','!','"','\n',':','\0'};
+
+ void split(char *line)
+ {
+  char *token = strtok(line, separators);
+   while(token != NULL) 
+  {
+    if(token[0] == '#')
+    {
+      int i;
+
+      Total_Ocorrencias++;
+
+      for(i = 1; token[i]; i++)
+      {
+        token[i] = tolower(token[i]);
+      }
+
+      insere(token); // Tem de confirmar se foi inserido ou se ja exestia, no caso de ser inserido incrementa o Total_Hashtags
+      compara_maior(token); // Compara com o Hashtag mais popular e confirma se passa a ser este o mais popular
+
+      }
+    token = strtok(NULL, separators);
+  }
+}
+
+
+
+ int main()
+ {
+  char input = 'U'; /* Inicialiazada a 'U' para entrar no ciclo
+             apenas 1 vez. Não corresponde a nenhuma
+             funcao. */
+
+   Total_Hashtags = 0;
+   Total_Ocorrencias = 0;
+             
+  char mensagem[MAX + 1];
+      
+    while ((input=getchar()) != 'x')
+    {
+                    
+      switch (input)
+      {
+        case 'a':
+          // Recebe a linha introduzida e divide-a em palavras separadas, selecionando os Hashtags
+          fgets(mensagem, MAX + 1, stdin);
+          split(mensagem);        
+          break;
+        
+        case 's':
+          
+          printf("%d %d\n", Total_Hashtags, Total_Ocorrencias);        
+          break;
+        
+        case 'm':   
+          
+          printf("%s %d\n",  Maior->tag, Maior->count);
+          break;
+        
+        case 'l':   
+          //Imprime todas as Hashtags e respetivos n de ocorrencias por ordem decrescente (em caso de empate, por ordem alfabetica)
+          int i;	
+          for (i = 0; i < Total_Hashtags; ++i)
+          {
+          	printf("%s %d\n", nome_hash(i), numero_ocor(i));
+          }
+          break;
+                    
+          
+        default:
+        
+          printf("Comando nao reconhecido, por favor reintroduza a operacao.\n");
+        
+      }
+    
+    }
+ 
+    return 0;
+    
+  }
+
