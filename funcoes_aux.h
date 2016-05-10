c26470edfd0b9f3b2daa58841a6046e714609be0
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2016-05-10 20:32:12 +0100

    Update funcoes_aux.h

--- funcoes_aux.h
+++ funcoes_aux.h
@@ -8,7 +8,11 @@
 extern int Total_Hashtags;
 extern link global_h;
 extern Item Maior;
+extern int len_vec;
+extern Item *vec_ord;
 
+void insert_vec_ord(Item item);
 void avalia_hash(char *token, int size_str);
+//void radix_sort(Item *vec_ord, int nr_hash, int M);
 
 #endif
