1c0316500eac80246a2404e04f76dafa9449bc6a
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2016-05-10 20:31:49 +0100

    Update funcoes_aux.c

--- funcoes_aux.c
+++ funcoes_aux.c
@@ -10,6 +10,7 @@ void avalia_hash(char *token, int size_str)
 		Item primeira_hash = new_item(token, size_str);
 	    global_h = NewTree(primeira_hash, NULL, NULL);
 	    Total_Hashtags++;
+	    insert_vec_ord(primeira_hash);
 	    Maior = global_h->item;
 	}
 
@@ -25,6 +26,7 @@ void avalia_hash(char *token, int size_str)
 			hash->count = 1;
 			global_h = insert(global_h, hash);
 			Total_Hashtags++;
+			insert_vec_ord(hash);
 			
 			if(Maior->count == 1)
 			{
@@ -44,31 +46,49 @@ void avalia_hash(char *token, int size_str)
 	}
 }
 
-
-/*
-void radix_sort(Item *a, int vec_len, int M)
-{   
-
-	int i, j, cnt[M];
-	Item *b = (Item*)malloc(sizeof(Item)*vec_len);
+/* para chamar: radix_sort(vec_ord, Total_Hashtags, Maior->count) 
+void radix_sort(Item *vec_ord, int nr_hash, int M)
+{
+	int i, j;
+	int *cnt = (int*)malloc(sizeof(int)*M);
+	Item *b = (Item*)malloc(sizeof(Item)*nr_hash);
 	
-	for (j = 0; j < M; j++) 
-		cnt[j] = 0;
-		
-	for (i = 0; i < vec_len; i++) 
-		cnt[a[i].count+1]++;
+	printf("1\n");
+
+	for (i = 0; i < nr_hash; i++) 
+		cnt[vec_ord[i]->count-1]++;
 	
+	printf("2\n");
+
 	for (j = 1; j < M; j++) 
 		cnt[j] += cnt[j-1];
 	
-	for (i = 0; i < vec_len; i++) 
-		b[cnt[a[i].count]++] = a[i];
+	printf("3\n");
+
+	for (i = 0; i < nr_hash; i++) 
+		b[cnt[vec_ord[i]->count]++] = vec_ord[i];
 	
-	for (i = 0; i < vec_len; i++)
+	printf("4\n");
+
+	for (i = 0; i < nr_hash; i++)
 	{
-		a[i] = b[i];
-		printf("count:%d tag:%s\n", a[i].count, a[i].tag);
+		vec_ord[i] = b[i];
+		//printf("count:%d tag:%s\n", a[i].count, a[i].tag);
 	}
 	
+	printf("5\n");
+
 	free(b);
-*/
+	free(cnt);
+} */
+
+void insert_vec_ord(Item item)
+{
+	if (Total_Hashtags >= len_vec)
+	{
+		vec_ord = (Item*) realloc(vec_ord, (len_vec+100)); 
+		len_vec += 100;
+	}
+
+	vec_ord[Total_Hashtags-1] = item;
+}
