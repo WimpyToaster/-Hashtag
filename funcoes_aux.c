2119e6d691667fcfa378c11143ac17ca8a2edac7
MIEGAS!!!! <miegas4@users.noreply.github.com>
2016-05-10 18:00:19 +0100

    Update funcoes_aux.c

--- funcoes_aux.c
+++ funcoes_aux.c
@@ -43,3 +43,32 @@ void avalia_hash(char *token, int size_str)
 		}
 	}
 }
+
+
+/*
+void radix_sort(Item *a, int vec_len, int M)
+{   
+
+	int i, j, cnt[M];
+	Item *b = (Item*)malloc(sizeof(Item)*vec_len);
+	
+	for (j = 0; j < M; j++) 
+		cnt[j] = 0;
+		
+	for (i = 0; i < vec_len; i++) 
+		cnt[a[i].count+1]++;
+	
+	for (j = 1; j < M; j++) 
+		cnt[j] += cnt[j-1];
+	
+	for (i = 0; i < vec_len; i++) 
+		b[cnt[a[i].count]++] = a[i];
+	
+	for (i = 0; i < vec_len; i++)
+	{
+		a[i] = b[i];
+		printf("count:%d tag:%s\n", a[i].count, a[i].tag);
+	}
+	
+	free(b);
+*/
