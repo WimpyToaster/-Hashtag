178704d4ac898d18de39b2acd1c426a524183171
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2016-05-10 17:34:37 +0100

    Update Item.h

--- Item.h
+++ Item.h
@@ -1,12 +1,20 @@
+#ifndef _ITEM_
+#define _ITEM_
+
+#include <string.h>
 #include <stdlib.h>
 
-#define eq(a,b) (strcmp(a.tag, b.tag) == 0)
-#define less(a,b) (strcmp(a.tag, b.tag) < 0)
+#define eq(a,b) (strcmp(a->tag, b->tag) == 0)
+#define less(a,b) (strcmp(a->tag, b->tag) < 0)
 	
 	
 typedef struct Hashtag{
 	
-	int count;
+	int count;	
 	char *tag;
 	
-}Item;
+}*Item;
+
+Item new_item(char *tag, int size_str);
+
+#endif
