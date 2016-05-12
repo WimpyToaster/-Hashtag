9168989ef200d5edd70562eb3c49f50424532ea9
MIEGAS!!!! <miegas4@users.noreply.github.com>
2016-05-12 12:45:10 -0700

    Update funcoes_aux.c

--- funcoes_aux.c
+++ funcoes_aux.c
@@ -26,6 +26,61 @@ void avalia_hash(char *token, int size_str)
 			hash->count = 1;
 			global_h = insert(global_h, hash);
 			Total_Hashtags++;
+			
+			
+/* 
+
+para chamar: 
+bucket_sort(vec_ord, Total_Hashtags, Maior->count);
+
+* isto so convem ser testado depois de o programa estar
+* completamente funcional, tem de se remover todas as
+* insercoes de Item's no vec_ord e fazer uma funcao que
+* faca isso a partir da arvore 
+* (esta é prototipo nao consegui testar por causa 
+*      de memory leaks) 		
+
+void tree_to_vec(link h)
+{
+	if (h == NULL)
+		return;
+ 
+	free_tree(h->l);
+	free_tree(h->r);
+	insert_vec_ord(h->item);
+}
+
+* alem disso nao ha muitas garantias que este bucket funcione
+* apesar de ja ter testado um bom bocado.
+
+void bucket_sort(Item *a, int nr_hash, int M)
+{
+	int i;
+	int *cnt = (int*)malloc(sizeof(int)*(M+1));
+	Item *b = (Item*)malloc(sizeof(Item)*nr_hash);
+	
+	for (i = 0; i < nr_hash; i++) 
+		cnt[a[i]->count + 1]++;
+		
+	for (i = 1; i <= M; i++) //aki tem a soma acumlutativa 
+		cnt[i] += cnt[i-1];
+		
+	for (i = 0; i < nr_hash; i++) 
+		b[cnt[a[i]->count]++] = a[i]; 
+	
+	for (i = 0; i < nr_hash; i++)
+	{
+		a[i] = b[(nr_hash - 1) - i]; 
+		 
+		// isto eh debug!!!! 
+		printf("tag:%s\ncount:%d\n\n",a[i]->tag,a[i]->count);
+	}
+	
+	free(b);
+	free(cnt);
+
+}
+*/
 			insert_vec_ord(hash);
 			
 			if(Maior->count == 1)
