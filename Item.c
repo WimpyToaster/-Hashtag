aed24c42b98dec1b58e95688c9c78272bd7ed759
WimpyToaster <antonio.terra@tecnico.ulisboa.pt>
2016-05-12 21:25:37 +0100

    Update Item.c

--- Item.c
+++ Item.c
@@ -4,8 +4,7 @@
 Item new_item(char *tag, int size_str)
 {
 	Item new = (Item) malloc(sizeof(struct Hashtag));
-
-	new->tag = (char*) malloc(sizeof(char)*size_str);
+	new->tag = (char*) malloc(sizeof(char)*size_str+1);
 	strcpy(new->tag, tag);
 	new->count = 1;
 
